9cbb5a2f6f1db5f65468f8a9ff3a9780391f41f5
NickRbk <nrybak94@gmail.com>
2017-01-23 21:47:45 -0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,21 +15,25 @@ app.use( express.static(publicPath) );
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'noreply@gmail.com',
-    text: 'Hello world..',
-    createdAt: 1234567
-  });
+  // socket.emit('newMessage', {
+  //   from: 'noreply@gmail.com',
+  //   text: 'Hello world..',
+  //   createdAt: 1234567
+  // });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
     console.log('Disconnected from client');
   });
-
-
 
 });
 
