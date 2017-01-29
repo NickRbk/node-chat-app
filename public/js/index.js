6c19ecafe279ffd00459c0a4188332fea5691048
NickRbk <nrybak94@gmail.com>
2017-01-28 16:18:09 -0800

    add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,10 @@
 const socket = io();
 
-socket.on('connect', function () {
-  console.log('Connected to server');
+socket.on('connect', () => console.log('Connected to server'));
 
-});
+socket.on('disconnect', () => console.log('Disconnected from server'));
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
-});
-
-socket.on('newMessage', function (message) {
+socket.on('newMessage', (message) => {
   console.log('newMessage', message);
 
   let li = jQuery('<li></li>');
@@ -18,13 +13,36 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e) {
+socket.on('newLocationMessage', (message) => {
+  let li = jQuery('<li></li>');
+  let a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', (e) => {
   e.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, data => console.log('Got it', data));
+});
 
+let locationButton = jQuery('#send-location');
+locationButton.on('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
 
+  navigator.geolocation.getCurrentPosition( (position) => {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, () => alert('Uable to fetch location'));
 });
